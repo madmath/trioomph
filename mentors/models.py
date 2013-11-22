9db9ca037ddba2f79f1d62709b75309ce4b64d26
Mathieu Perreault <mathieu.perreault@gmail.com>
2013-11-22 00:23:58 -0500

    blanks

--- mentors/models.py
+++ mentors/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import User
 
 class MentorProfile(models.Model):
   user = models.OneToOneField(User, related_name="profile", null=True, blank=True)
-  bio = models.TextField(verbose_name="Short biography")
-  oneliner = models.TextField(verbose_name="Sentence that represents well. Dream, one-liner, quote, etc.")
+  bio = models.TextField(verbose_name="Short biography", blank=True)
+  oneliner = models.TextField(verbose_name="Sentence that represents well. Dream, one-liner, quote, etc.", blank=True)
   thumbnail = models.ImageField(upload_to="thumbs/", blank=True, null=True)
 
   def __unicode__(self):
     return u"%s %s" % (self.user.first_name, self.user.last_name)
 
 class VideoLink(models.Model):
-  title = models.CharField(max_length=1000)
-  description = models.TextField(verbose_name="Video description")
-  embed_code = models.TextField(verbose_name="YouTube or Vimeo embed code (iframe)")
-  video_id = models.CharField(max_length=1000)
+  title = models.CharField(max_length=1000, blank=True)
+  description = models.TextField(verbose_name="Video description", blank=True)
+  embed_code = models.TextField(verbose_name="YouTube or Vimeo embed code (iframe)", blank=True)
+  video_id = models.CharField(max_length=1000, blank=True)
   added_on = models.DateTimeField(auto_now_add=True)
   owner_profile = models.ForeignKey(MentorProfile)
 
@@ -25,8 +25,8 @@ class VideoLink(models.Model):
 
 class Conference(models.Model):
   title = models.CharField(max_length=1000)
-  description = models.TextField(verbose_name="Conference description")
-  link = models.CharField(max_length=1000, verbose_name="Link to more information about the conference")
+  description = models.TextField(verbose_name="Conference description", blank=True)
+  link = models.CharField(max_length=1000, verbose_name="Link to more information about the conference", blank=True)
   date = models.DateTimeField(verbose_name="When the conference is taking place")
   owner_profile = models.ForeignKey(MentorProfile)
 
@@ -49,4 +49,4 @@ class StudentProfile(models.Model):
   user = models.ForeignKey(User, blank=True, unique=True, verbose_name='user')
   choices = models.ManyToManyField(Choices)
   school = models.CharField(max_length=100)
-  year = models.CharField(max_length=25)
+  year = models.CharField(max_length=25, blank=True)
